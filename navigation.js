abec4f7e4326ce2995d7593167d59bf8af928127
Eric Silberberg <72359394+esilberberg@users.noreply.github.com>
2022-06-08 12:49:31 -0400

    upload v 5.0

---
+++ navigation.js
@@ -0,0 +1,14 @@
+// Controls navigation tab elements in main section
+function openSubject(evt, subject) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(subject).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
